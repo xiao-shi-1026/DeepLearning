7745c5c92c863780f22f559c6909197b1aa3ca12
Xiao Shi <sx1194217713@gmail.com>
2024-03-24 11:45:47 -0400

    finish HW3 part1

--- HW3_RNN/CTC/CTCDecoding.py
+++ HW3_RNN/CTC/CTCDecoding.py
@@ -114,32 +114,80 @@ class BeamSearchDecoder(object):
 
         """
 
+        blank = 0
         T = y_probs.shape[1]
         bestPath, FinalPathScore = None, None
-        for t in range(T):
-            if t == 0:
-                paths = [([], 1)]
-            else:
-                new_paths = []
-                for path, score in paths:
-                    for i in range(y_probs.shape[0]):
-                        new_path = path + [i]
-                        new_score = score * y_probs[i, t, 0]
-                        new_paths.append((new_path, new_score))
-                new_paths = sorted(new_paths, key=lambda x: x[1], reverse=True)
-                paths = new_paths[:self.beam_width]
-        bestPath, FinalPathScore = paths[0]
+        
+        # initialize beam -> list of lists e.g. [[2], [1]]
+        # Each lists inside the list is a path. Each number represents the index of the symbol in the symbol_set
+        path = [[i] for i in np.argsort(y_probs[:, 0, :].flatten())[::-1][:self.beam_width]]
 
-        # compress
-        compressed = []
-        for i in range(len(bestPath)):
-            if bestPath[i] != 0:
-                if i == 0 or bestPath[i] != bestPath[i - 1]:
-                    compressed.append(bestPath[i])
-        bestPath = []
-        for i in compressed:
-            bestPath.append(self.symbol_set[i - 1])
+        # initialize probability for each path -> list of lists
+        # Each lists inside the list is a prob of that path. The index of prob is the same as the index of the path
+
+        prob = [i for i in np.sort(y_probs[:, 0, :].flatten())[::-1][:self.beam_width]]
+
+        # update the path and prob for each time step
+        for t in range(1, T):
+            # new path could only be the highest beam_width th. Use the same method to initialize new_path
+            new_path = []
+            for i in range(len(path)):
+                for j in range(len(y_probs[:, t, :].flatten())):
+                    copypath = path[i].copy()
+                    copypath.append(j)
+                    new_path.append(copypath)
+            new_prob = [i * j for i in prob for j in y_probs[:, t, :].flatten()]
+
+            # compress the path. The sequence of if loop is important, as we don't want to
+            # delete blank in the middle of the path but only delete the last blank.
+            for tmp_path in new_path:
+                
+                if len(tmp_path) >= 2 and tmp_path[-2] == blank:
+                    tmp_path.pop(-2)
+                    continue
+                
+                if t == T - 1 and tmp_path[-1] == blank:
+                    tmp_path.pop()
+                    continue
+
+                if tmp_path[-1] == tmp_path[-2]:
+                    tmp_path.pop()
+                    continue
+
+                
+
+            path_dict = {}
+            
+            for i in range(len(new_path)):
+                if tuple(new_path[i].copy()) not in path_dict:
+                    path_dict[tuple(new_path[i].copy())] = new_prob[i].copy()
+                else:
+                    path_dict[tuple(new_path[i].copy())] += new_prob[i].copy()
+            before_prune = path_dict.copy()
+
+            # return top beam_width paths
+            path = []
+            prob = []
+            for i in range(self.beam_width):
+                max_key = max(path_dict, key=path_dict.get)
+                path.append(list(max_key))
+                prob.append(path_dict[max_key])
+                del path_dict[max_key]
+            
+        bestPath = path[np.argmax(prob)]
+        
+        # delete 0 in bestPath
+        bestPath = [i for i in bestPath if i != 0]
+        bestPath = [self.symbol_set[i - 1] for i in bestPath]
         bestPath = ''.join(bestPath)
+       
+        # change the key of finalpathscore to string
+        FinalPathScore = {}
+
+        for key, value in before_prune.items():
+            newkey = [self.symbol_set[i - 1] if i != 0 else '' for i in key] # The HW3 requires the blank to be ''
+            newkey = ''.join(newkey)
+            FinalPathScore[newkey] = value
 
         return bestPath, FinalPathScore
 
